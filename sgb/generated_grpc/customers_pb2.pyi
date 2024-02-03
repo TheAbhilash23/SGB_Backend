415ed0f60f776a479ec19afbbca97256e6f1e96c
TheAbhilash23 <theak.92@gmail.com>
2024-02-03 14:10:43 +0530

    feature: added customer protos in sgb service

---
+++ sgb/generated_grpc/customers_pb2.pyi
@@ -0,0 +1,43 @@
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Optional as _Optional
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class ListRequest(_message.Message):
+    __slots__ = []
+    def __init__(self) -> None: ...
+
+class CustomerRetrieveRequest(_message.Message):
+    __slots__ = ["customer_id"]
+    CUSTOMER_ID_FIELD_NUMBER: _ClassVar[int]
+    customer_id: int
+    def __init__(self, customer_id: _Optional[int] = ...) -> None: ...
+
+class CustomerUserRetrieveRequest(_message.Message):
+    __slots__ = ["user_id"]
+    USER_ID_FIELD_NUMBER: _ClassVar[int]
+    user_id: int
+    def __init__(self, user_id: _Optional[int] = ...) -> None: ...
+
+class CustomerCreateRequest(_message.Message):
+    __slots__ = ["user_id", "name", "email"]
+    USER_ID_FIELD_NUMBER: _ClassVar[int]
+    NAME_FIELD_NUMBER: _ClassVar[int]
+    EMAIL_FIELD_NUMBER: _ClassVar[int]
+    user_id: int
+    name: str
+    email: str
+    def __init__(self, user_id: _Optional[int] = ..., name: _Optional[str] = ..., email: _Optional[str] = ...) -> None: ...
+
+class CustomerData(_message.Message):
+    __slots__ = ["user_id", "name", "id", "email"]
+    USER_ID_FIELD_NUMBER: _ClassVar[int]
+    NAME_FIELD_NUMBER: _ClassVar[int]
+    ID_FIELD_NUMBER: _ClassVar[int]
+    EMAIL_FIELD_NUMBER: _ClassVar[int]
+    user_id: int
+    name: str
+    id: int
+    email: str
+    def __init__(self, user_id: _Optional[int] = ..., name: _Optional[str] = ..., id: _Optional[int] = ..., email: _Optional[str] = ...) -> None: ...
